8d8950bbd7a468e621709e2dc2a47f93b2e15f54
Will Dengler <wdengler@jibe.com>
2019-01-27 14:54:36 -0500

    Updating tests to match changes made to the programs functionality

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in $(seq 1 `ls inputs/public* | wc -l`);do
-  echo $i: $(../main.byte inputs/public_$i.rubevm | diff -Bw outputs/public_$i.out -);
+  echo $i: $(../main.byte inputs/public_$i.evm | diff -Bw outputs/public_$i.out -);
 done
